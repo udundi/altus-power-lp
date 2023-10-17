148948c9800be2eeaa51d9ed96964b4cad00d262
Nathan Parker <nathan@Nathans-MacBook-Air-2.local>
2023-10-17 14:00:44 -0500

    added CompleteRegistration facebook conversion api

--- src/stores/Forms.js
+++ src/stores/Forms.js
@@ -17,26 +17,26 @@ export const useForms = defineStore('forms', () => {
     state.contact.loading = true
 
     try {
-      // const res = await fetch(import.meta.env.VITE_EMAIL_URL, {
-      //   method: 'POST',
-      //   headers: {
-      //     'Content-Type': 'application/json'
-      //   },
-      //   body: JSON.stringify({
-      //     formType: data.formType,
-      //     firstName: data.firstName,
-      //     lastName: data.lastName,
-      //     email: data.email,
-      //     // receiveEmails: data.receiveEmails ? 'Yes' : 'No'
-      //   })
-      // })
+      const res = await fetch('https://bnjx3642fumzl5ose6akxhgwiq0rpjcy.lambda-url.us-east-1.on.aws/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          event_name: 'CompleteRegistration',
+          first_name: data.firstName,
+          last_name: data.lastName,
+          email: data.email,
+          phone: data.phone
+        })
+      })
 
-      // const result = await res.json()
-      // console.log('result >>', result)
+      const result = await res.json()
+      console.log('result >>', result)
 
-      // state.contact.success = true
+      state.contact.success = true
       // + '&refCode=' + data.refCode
-      window.location = '//app.altuspower.com/communitysolar/onboarding/availability?f=' + data.firstName + '&l=' + data.lastName + '&e=' + data.email + '&p=' + data.phone
+      // window.location = '//app.altuspower.com/communitysolar/onboarding/availability?f=' + data.firstName + '&l=' + data.lastName + '&e=' + data.email + '&p=' + data.phone
     } catch (error) {
       console.log('error >>', error)
       state.contact.error = error
