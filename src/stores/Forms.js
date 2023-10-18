b3ba2190ec454b79c99636e627882ce00319fbbc
Nathan Parker <nathan@Nathans-MacBook-Air-2.local>
2023-10-18 10:48:09 -0500

    updated forms data

--- src/stores/Forms.js
+++ src/stores/Forms.js
@@ -17,28 +17,28 @@ export const useForms = defineStore('forms', () => {
     state.contact.loading = true
 
     try {
-    //   const res = await fetch('https://e99i3hntn7.execute-api.us-east-1.amazonaws.com/default/altusPowerAnalytics', {
-    //     method: 'POST',
-    //     headers: {
-    //       'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify({
-    //       event_name: 'CompleteRegistration',
-    //       first_name: data.firstName,
-    //       last_name: data.lastName,
-    //       email: data.email,
-    //       phone: data.phone
-    //     })
-    //   })
+      const res = await fetch('https://e99i3hntn7.execute-api.us-east-1.amazonaws.com/default/altusPowerAnalytics', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          event_name: 'CompleteRegistration',
+          first_name: data.firstName,
+          last_name: data.lastName,
+          email: data.email,
+          phone: data.phone
+        })
+      })
 
-    //   if (!res.ok) {
-    //     throw new Error(`HTTP error! Status: ${res.status}`)
-    //   }
+      if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`)
+      }
 
-    //   const result = await res.json()
-    //   console.log('result >>', result)
+      const result = await res.json()
+      console.log('result >>', result)
 
-    //   state.contact.success = true
+      state.contact.success = true
       // + '&refCode=' + data.refCode
       window.location = '//app.altuspower.com/communitysolar/onboarding/availability?f=' + data.firstName + '&l=' + data.lastName + '&e=' + data.email + '&p=' + data.phone
     } catch (error) {
