30fd33460c8360f69493471f79fbb238538f1cd7
Nathan Parker <nathan@Nathans-MacBook-Air-2.local>
2023-10-18 13:26:51 -0500

    fix url redirect

--- src/stores/Forms.js
+++ src/stores/Forms.js
@@ -17,27 +17,27 @@ export const useForms = defineStore('forms', () => {
     state.contact.loading = true
 
     try {
-      const res = await fetch('https://e99i3hntn7.execute-api.us-east-1.amazonaws.com/default/altusPowerAnalytics', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*'
-        },
-        body: JSON.stringify({
-          event_name: 'Subscribe',
-          first_name: data.firstName,
-          last_name: data.lastName,
-          email: data.email,
-          phone: data.phone
-        })
-      })
+      // const res = await fetch('https://e99i3hntn7.execute-api.us-east-1.amazonaws.com/default/altusPowerAnalytics', {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //     'Access-Control-Allow-Origin': '*'
+      //   },
+      //   body: JSON.stringify({
+      //     event_name: 'Subscribe',
+      //     first_name: data.firstName,
+      //     last_name: data.lastName,
+      //     email: data.email,
+      //     phone: data.phone
+      //   })
+      // })
 
-      const result = await res.json()
-      console.log('result >>', result)
+      // const result = await res.json()
+      // console.log('result >>', result)
 
       state.contact.success = true
       // + '&refCode=' + data.refCode
-      // window.location = '//app.altuspower.com/communitysolar/onboarding/availability?f=' + data.firstName + '&l=' + data.lastName + '&e=' + data.email + '&p=' + data.phone
+      window.location = '//app.altuspower.com/communitysolar/onboarding/availability?f=' + data.firstName + '&l=' + data.lastName + '&e=' + data.email + '&p=' + data.phone
     } catch (error) {
       console.log('error >>', error)
       console.log('error.message >>', error.message)
