f360d19d05b664dfb695d9fcfa02988981957658
Nathan Parker <nathan@Nathans-MacBook-Air-2.local>
2023-10-18 13:29:19 -0500

    updated redirect and POST

--- src/stores/Forms.js
+++ src/stores/Forms.js
@@ -17,23 +17,23 @@ export const useForms = defineStore('forms', () => {
     state.contact.loading = true
 
     try {
-      // const res = await fetch('https://e99i3hntn7.execute-api.us-east-1.amazonaws.com/default/altusPowerAnalytics', {
-      //   method: 'POST',
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //     'Access-Control-Allow-Origin': '*'
-      //   },
-      //   body: JSON.stringify({
-      //     event_name: 'Subscribe',
-      //     first_name: data.firstName,
-      //     last_name: data.lastName,
-      //     email: data.email,
-      //     phone: data.phone
-      //   })
-      // })
+      const res = await fetch('https://e99i3hntn7.execute-api.us-east-1.amazonaws.com/default/altusPowerAnalytics', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+          event_name: 'Subscribe',
+          first_name: data.firstName,
+          last_name: data.lastName,
+          email: data.email,
+          phone: data.phone
+        })
+      })
 
-      // const result = await res.json()
-      // console.log('result >>', result)
+      const result = await res.json()
+      console.log('result >>', result)
 
       state.contact.success = true
       // + '&refCode=' + data.refCode
