b92c22c9fcb4e00fed50df503585978d13119d32
nathanparker <nathan@udundi.com>
2024-02-01 03:02:54 +0800

    update to altus power domain and refs

--- src/stores/Forms.js
+++ src/stores/Forms.js
@@ -37,7 +37,8 @@ export const useForms = defineStore('forms', () => {
 
       state.contact.success = true
       // + '&refCode=' + data.refCode
-      window.location = '//join.altuspower.com/communitysolar/onboarding/availability?f=' + data.firstName + '&l=' + data.lastName + '&e=' + data.email + '&p=' + data.phone
+      // window.location = '//join.altuspower.com/communitysolar/onboarding/availability?f=' + data.firstName + '&l=' + data.lastName + '&e=' + data.email + '&p=' + data.phone
+      window.location = '//app.altuspower.com/communitysolar/service/introduction?firstName=' + data.firstName + '&lastName=' + data.lastName + '&email=' + data.email + '&phoneNumber=' + data.phone
     } catch (error) {
       console.log('error >>', error)
       console.log('error.message >>', error.message)
